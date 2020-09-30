998940b2ccf2b3b6ac9aa12aac9ce9f964b3b7b2
Josh <joshuscurtis@gmail.com>
2020-09-30 17:34:22 +0100

    add auth

--- server/index.js
+++ server/index.js
@@ -461,7 +461,7 @@ app.post('/updateAvg', (req,res) => {
 	})
 	 
 // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
+  app.get('*', myAuth, function(request, response) {
     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
   });
 	
