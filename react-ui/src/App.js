abd9415ec29f539f304a5572102e44efb28099d7
Josh <joshuscurtis@gmail.com>
2020-10-04 21:22:04 +0100

    App.js

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -343,7 +343,7 @@ return (
               href={jump}
               onClick={handleClick}
             >
-			  {jump==="#table" ? "Table" :  "Takeaway"}
+			  {jump==="#table" ? "Takeaway" :  "Table"}
             </Button>
           </div>
           <SettingsDialog />
